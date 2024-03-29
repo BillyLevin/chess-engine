230c612a738ccdc1eee76da459069720c1189dd1
Billy Levin <billy.levin97@gmail.com>
2024-03-29 15:49:46 +0000

    print full board

--- engine.c
+++ engine.c
@@ -46,6 +46,7 @@ typedef enum {
   BLACK_ROOK,
   BLACK_QUEEN,
   BLACK_KING,
+  EMPTY,
 } piece_t;
 
 const wchar_t PIECE_UNICODE[12] = {0x2659, 0x2658, 0x2657, 0x2656,
@@ -106,18 +107,104 @@ board_t *board_new() {
   return board;
 }
 
+void board_print(board_t *board) {
+  uint64_t bitboards[12] = {
+      board->white_pawns, board->white_knights, board->white_bishops,
+      board->white_rooks, board->white_queens,  board->white_king,
+      board->black_pawns, board->black_knights, board->black_bishops,
+      board->black_rooks, board->black_queens,  board->black_king,
+  };
+
+  for (int rank = 7; rank >= 0; rank--) {
+    for (int file = 0; file < 8; file++) {
+      if (file == 0) {
+        printf("%3d", rank + 1);
+      }
+
+      // https://www.chessprogramming.org/Square_Mapping_Considerations#Deduction_on_Files_and_Ranks
+      int square = (rank * 8) + file;
+
+      piece_t piece = EMPTY;
+
+      for (int i = 0; i < 12; i++) {
+        uint64_t occupation = (bitboards[i] & (1ULL << square));
+
+        if (occupation > 0) {
+          piece = i;
+          break;
+        }
+      }
+
+      if (piece != EMPTY) {
+        piece_print(piece);
+      } else {
+        printf("%3d", 0);
+      }
+    }
+
+    printf("\n");
+  }
+
+  printf("   ");
+  char ranks[8] = "ABCDEFGH";
+  for (int i = 0; i < 8; i++) {
+    printf("%3c", ranks[i]);
+  }
+  printf("\n\n");
+}
+
+void board_set_starting_position(board_t *board) {
+  board->white_pawns |= 1ULL << A2;
+  board->white_pawns |= 1ULL << B2;
+  board->white_pawns |= 1ULL << C2;
+  board->white_pawns |= 1ULL << D2;
+  board->white_pawns |= 1ULL << E2;
+  board->white_pawns |= 1ULL << F2;
+  board->white_pawns |= 1ULL << G2;
+  board->white_pawns |= 1ULL << H2;
+
+  board->white_knights |= 1ULL << B1;
+  board->white_knights |= 1ULL << G1;
+
+  board->white_bishops |= 1ULL << C1;
+  board->white_bishops |= 1ULL << F1;
+
+  board->white_rooks |= 1ULL << A1;
+  board->white_rooks |= 1ULL << H1;
+
+  board->white_queens |= 1ULL << D1;
+  board->white_king |= 1ULL << E1;
+
+  board->black_knights |= 1ULL << B8;
+  board->black_knights |= 1ULL << G8;
+
+  board->black_pawns |= 1ULL << A7;
+  board->black_pawns |= 1ULL << B7;
+  board->black_pawns |= 1ULL << C7;
+  board->black_pawns |= 1ULL << D7;
+  board->black_pawns |= 1ULL << E7;
+  board->black_pawns |= 1ULL << F7;
+  board->black_pawns |= 1ULL << G7;
+  board->black_pawns |= 1ULL << H7;
+
+  board->black_knights |= 1ULL << B8;
+  board->black_knights |= 1ULL << G8;
+
+  board->black_bishops |= 1ULL << C8;
+  board->black_bishops |= 1ULL << F8;
+
+  board->black_rooks |= 1ULL << A8;
+  board->black_rooks |= 1ULL << H8;
+
+  board->black_queens |= 1ULL << D8;
+  board->black_king |= 1ULL << E8;
+}
+
 int main() {
   board_t *board = board_new();
 
-  bitboard_print(board->white_pawns, WHITE_PAWN);
-
-  board->white_pawns |= 1ULL << A2;
-
-  bitboard_print(board->white_pawns, WHITE_PAWN);
-
-  board->white_pawns |= 1ULL << E6;
-
-  bitboard_print(board->white_pawns, WHITE_PAWN);
+  board_set_starting_position(board);
+  board_print(board);
 
   return EXIT_SUCCESS;
 }
