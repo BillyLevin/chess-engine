8acae119b26a9fde5d3428e6cdccb5e40a6446f3
Billy Levin <billy.levin97@gmail.com>
2024-03-29 07:09:37 +0000

    add board struct

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	cc -std=c99 -Wall engine.c -o engine.out
--- engine.c
+++ engine.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-int main() {
-  printf("this will be a chess engine one day\n");
-  return EXIT_SUCCESS;
+typedef struct {
+  uint64_t white_pawns;
+  uint64_t white_knights;
+  uint64_t white_bishops;
+  uint64_t white_rooks;
+  uint64_t white_queens;
+  uint64_t white_king;
+
+  uint64_t black_pawns;
+  uint64_t black_knights;
+  uint64_t black_bishops;
+  uint64_t black_rooks;
+  uint64_t black_queens;
+  uint64_t black_king;
+} board_t;
+
+board_t *board_new() {
+  board_t *board = malloc(sizeof(board_t));
+  return board;
 }
+
+int main() { return EXIT_SUCCESS; }
