9939e24eb719de564965b59174df6276d1c9d77f
Billy Levin <billy.levin97@gmail.com>
2024-03-29 08:22:24 +0000

    add square types

--- engine.c
+++ engine.c
@@ -18,16 +18,29 @@ typedef struct {
   uint64_t black_king;
 } board_t;
 
+// clang-format off
+typedef enum {
+  A1, B1, C1, D1, E1, F1, G1, H1,
+  A2, B2, C2, D2, E2, F2, G2, H2,
+  A3, B3, C3, D3, E3, F3, G3, H3,
+  A4, B4, C4, D4, E4, F4, G4, H4,
+  A5, B5, C5, D5, E5, F5, G5, H5,
+  A6, B6, C6, D6, E6, F6, G6, H6,
+  A7, B7, C7, D7, E7, F7, G7, H7,
+  A8, B8, C8, D8, E8, F8, G8, H8
+} square_t;
+// clang-format on
+
 void bitboard_print(uint64_t bitboard) {
-  for (int rank = 0; rank < 8; rank++) {
+  for (int rank = 7; rank >= 0; rank--) {
     for (int file = 0; file < 8; file++) {
       if (file == 0) {
-        printf("%3d", 8 - rank);
+        printf("%3d", rank);
       }
 
       // https://www.chessprogramming.org/Square_Mapping_Considerations#Deduction_on_Files_and_Ranks
       int square = (rank * 8) + file;
-      int occupation = (bitboard & (1ULL < square)) ? 1 : 0;
+      int occupation = (bitboard & (1ULL << square)) ? 1 : 0;
 
       printf("%3d", occupation);
     }
@@ -40,31 +53,35 @@ void bitboard_print(uint64_t bitboard) {
   for (int i = 0; i < 8; i++) {
     printf("%3c", ranks[i]);
   }
-  printf("\n");
+  printf("\n\n");
 };
 
 board_t *board_new() {
   board_t *board = malloc(sizeof(board_t));
 
-  board->white_pawns = 0;
-  board->white_knights = 0;
-  board->white_bishops = 0;
-  board->white_rooks = 0;
-  board->white_queens = 0;
-  board->white_king = 0;
+  board->white_pawns = 0ULL;
+  board->white_knights = 0ULL;
+  board->white_bishops = 0ULL;
+  board->white_rooks = 0ULL;
+  board->white_queens = 0ULL;
+  board->white_king = 0ULL;
 
-  board->black_pawns = 0;
-  board->black_knights = 0;
-  board->black_bishops = 0;
-  board->black_rooks = 0;
-  board->black_queens = 0;
-  board->black_king = 0;
+  board->black_pawns = 0ULL;
+  board->black_knights = 0ULL;
+  board->black_bishops = 0ULL;
+  board->black_rooks = 0ULL;
+  board->black_queens = 0ULL;
+  board->black_king = 0ULL;
 
   return board;
 }
 
 int main() {
   board_t *board = board_new();
+
+  bitboard_print(board->white_pawns);
+
+  board->white_pawns |= 1ULL << A2;
 
   bitboard_print(board->white_pawns);
 
