54a40960b89dc912f5c2b39a6d0ab63efc32a969
Billy Levin <billy.levin97@gmail.com>
2024-03-30 08:02:21 +0000

    add en passant square to board struct

--- engine.c
+++ engine.c
@@ -15,6 +15,19 @@ enum {
   BLACK_QUEEN_CASTLE = 8
 };
 
+// clang-format off
+typedef enum {
+  A1, B1, C1, D1, E1, F1, G1, H1,
+  A2, B2, C2, D2, E2, F2, G2, H2,
+  A3, B3, C3, D3, E3, F3, G3, H3,
+  A4, B4, C4, D4, E4, F4, G4, H4,
+  A5, B5, C5, D5, E5, F5, G5, H5,
+  A6, B6, C6, D6, E6, F6, G6, H6,
+  A7, B7, C7, D7, E7, F7, G7, H7,
+  A8, B8, C8, D8, E8, F8, G8, H8, NO_SQUARE
+} square_t;
+// clang-format on
+
 typedef struct {
   uint64_t white_pawns;
   uint64_t white_knights;
@@ -35,20 +48,9 @@ typedef struct {
   side_t side;
 
   int castle_rights;
-} board_t;
 
-// clang-format off
-typedef enum {
-  A1, B1, C1, D1, E1, F1, G1, H1,
-  A2, B2, C2, D2, E2, F2, G2, H2,
-  A3, B3, C3, D3, E3, F3, G3, H3,
-  A4, B4, C4, D4, E4, F4, G4, H4,
-  A5, B5, C5, D5, E5, F5, G5, H5,
-  A6, B6, C6, D6, E6, F6, G6, H6,
-  A7, B7, C7, D7, E7, F7, G7, H7,
-  A8, B8, C8, D8, E8, F8, G8, H8
-} square_t;
-// clang-format on
+  square_t en_passant_square;
+} board_t;
 
 typedef enum {
   WHITE_PAWN,
@@ -124,6 +126,7 @@ board_t *board_new() {
   board->fifty_move_rule_count = 0;
   board->side = WHITE;
   board->castle_rights = 0;
+  board->en_passant_square = NO_SQUARE;
 
   return board;
 }
