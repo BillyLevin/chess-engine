bd0abee714cbab2e02b633070f3b8363f7427568
Billy Levin <billy.levin97@gmail.com>
2024-03-30 08:10:38 +0000

    print en passant square

--- engine.c
+++ engine.c
@@ -26,6 +26,17 @@ typedef enum {
   A7, B7, C7, D7, E7, F7, G7, H7,
   A8, B8, C8, D8, E8, F8, G8, H8, NO_SQUARE
 } square_t;
+
+const char SQUARE_TO_READABLE[][3] = {
+  "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
+  "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
+  "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3",
+  "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4",
+  "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5",
+  "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6",
+  "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
+  "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8",
+};
 // clang-format on
 
 typedef struct {
@@ -188,6 +199,10 @@ void board_print(board_t *board) {
          board->castle_rights & BLACK_KING_CASTLE ? "yes" : "no");
   printf("  - Black queenside: %s\n",
          board->castle_rights & BLACK_QUEEN_CASTLE ? "yes" : "no");
+  printf("En passant square: %s\n",
+         board->en_passant_square == NO_SQUARE
+             ? "-"
+             : SQUARE_TO_READABLE[board->en_passant_square]);
 }
 
 #define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
